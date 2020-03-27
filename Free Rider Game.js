e075a5adb0735484ed24df4c5178edc9911e90bf
Eric Hilary Smith <erichilarysmith@outlook.com>
2020-03-27 06:15:05 -0500

    Add files via upload

---
+++ Free Rider Game.js
@@ -0,0 +1,124 @@
+// Variables used by Scriptable.
+// These must be at the very top of the file. Do not edit.
+// always-run-in-app: true; icon-color: deep-brown;
+// icon-glyph: praying-hands; share-sheet-inputs: plain-text, image, url, file-url;
+// SEELAH Media Works
+// Calamityville Series
+// DePaul University Chicago
+// University of Illinois
+// COSINE
+// Free Rider the Chicago Poverty Challenge
+// CONFESS 2 Become Financial Services
+// CONFESS 2 Become Financial Education Curriculum
+var nonplayerBusinessAssociate;
+nonplayerBusinessAssociate;
+var nonplayerAnomie;
+nonplayerAnomie;
+var dilemmaVolunteer;
+dilemmaVolunteer;
+var nonplayerGeneTFracking;
+nonplayerGeneTFracking;
+var player;
+player;
+var playerFamily;
+playerFamily;
+var playerProgenitorMother;
+playerProgenitorMother;
+var playerProgenitorFather;
+playerProgenitorFather;
+var playerOffSpringSon;
+playerOffSpringSon;
+var playerOffSpringDaughter;
+playerOffSpringDaughter;
+var playerIndividual;
+playerIndividual;
+var playerLeviathanFLucre;
+playerLeviathanFLucre;
+var playergroups;
+playergroups;
+var playergroupsSpecialInterests;
+playergroupsSpecialInterests;
+var nonplayerfactions;
+nonplayerfactions;
+var playerfactionsPolitical;
+playerfactionsPolitical;
+var organizations;
+organizations;
+var organizationsCommunity;
+organizationsCommunity;
+var organizationPublicNuisanceCityNiche;
+organizationPublicNuisanceCityNiche;
+var nonPlayer;
+nonPlayer;
+var nonplayerOEDIPUSSmalltalk;
+nonplayerOEDIPUSSmalltalk;
+var nonPlayer = ["nonplayerFreeRider", "nonplayerAnomie", "nonplayerFamiliarStranger", "nonplayerOEDIPUSSmalltalk"];
+var nonPlayersWeapons;
+nonPlayersWeapons;
+var weaponsLifeShocks;
+weaponsLifeShocks;
+var lifeshockDivorce;
+lifeshockDivorce;
+var lifeshockEviction;
+lifeshockEviction;
+var lifeshockBankruptcy;
+lifeshockBankruptcy;
+var lifeshockUnemployment;
+lifeshockUnemployment;
+var lifeshockHomeArson;
+lifeshockHomeArson;
+var lifeshockCrime;
+lifeshockCrime;
+var lifeshockCrimeHate;
+lifeshockCrimeHate;
+var githubGallupCrime;
+githubGallupCrime;
+var lifeshockHomeInvasion;
+lifeshockHomeInvasion;
+var lifeshockPrison;
+lifeshockPrison;
+var lifeshockIllness;
+lifeshockIllness;
+var lifeshockDomesticViolence;
+lifeshockDomesticViolence;
+var lifeshockRape;
+lifeshockRape;
+var weaponsLifeShocks = ["lifeshockDivorce", "lifeshockEviction", "lifeshockBankruptcy", "lifeshockUnemployment", "lifeshockHomeArson", "lifeshockHomeInvasion", "lifeshockPrison", "lifeshockIllness", "lifeshockDomesticViolence", "lifeshockRape"];
+for (var i = 0;  i < weaponsLifeShocks.length;  i++) {
+  console.log("Nonplayers weapons on Free Rider Game are " + weaponsLifeShocks[i] + ".");
+  }
+var dystopia;
+dystopia;
+var githubdystopia;
+githubdystopia;
+var githubGallupCrime;
+githubGallupCrime;
+var ViciousCyclePoverty;
+ViciousCyclePoverty;
+var facebook;
+facebook;
+var youtube;
+youtube;
+var pinterest;
+pinterest;
+var riskExistential;
+riskExistential;
+var riskAversion;
+riskAversion;
+var choiceSocial;
+choiceSocial;
+var choicePublic;
+choicePublic;
+var tradeFair;
+tradeFair;
+var shareFair;
+shareFair;
+var fallacyGamblers;
+fallacyGamblers;
+var fallacyEcological;
+fallacyEcological;
+var ruinsGamblers;
+ruinsGamblers;
+var fracking;
+fracking;
+var fracking = [];
